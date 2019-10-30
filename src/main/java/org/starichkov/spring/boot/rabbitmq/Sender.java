5791a84bb8653d46ca3f1756d6172b96e851fda0
Vadim Starichkov <starichkovva@gmail.com>
2019-10-30 18:17:07 +0300

    Added sending correlation data for confirmation callback

--- src/main/java/org/starichkov/spring/boot/rabbitmq/Sender.java
+++ src/main/java/org/starichkov/spring/boot/rabbitmq/Sender.java
@@ -2,10 +2,13 @@ package org.starichkov.spring.boot.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * @author Vadim Starichkov
@@ -25,8 +28,13 @@ public class Sender {
 
     @Scheduled(fixedDelay = 5000, initialDelay = 1000)
     public void send() {
-        String message = "Hello World!";
-        this.template.convertAndSend(queue, message);
+        String messageUid = generateUid();
+        String message = String.format("Hello World! [uid=%s]", messageUid);
+        this.template.convertAndSend(queue, (Object) message, new CorrelationData(messageUid));
         log.info("Message sent: {}", message);
+    }
+
+    private static String generateUid() {
+        return UUID.randomUUID().toString();
     }
 }
